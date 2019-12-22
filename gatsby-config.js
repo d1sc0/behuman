14acbc954959e2602d9f05c3c64104099415d470
d1sc0 <ilovediscostu@gmail.com>
2019-12-22 23:26:48 +0000

    updates to style and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 640,
+              maxWidth: 920,
+              linkImagesToOriginal: false,
             },
           },
           {
